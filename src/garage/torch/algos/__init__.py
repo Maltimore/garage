08774f2e15e13b0a3e79563c4d2a674eb2be6550
Utkarsh Patel <utkarshjp7@Gmail.com>
2019-11-17 01:44:27 -0800

    Remove optimizer_args from torch algos

--- src/garage/torch/algos/__init__.py
+++ src/garage/torch/algos/__init__.py
@@ -1,4 +1,7 @@
 """PyTorch algorithms."""
+from garage.torch.algos._utils import (  # noqa: F401
+    _compute_advantages, _Default, _filter_valids, _make_optimizer,
+    _pad_to_last)
 from garage.torch.algos.ddpg import DDPG
 from garage.torch.algos.vpg import VPG
 from garage.torch.algos.ppo import PPO  # noqa: I100
--- src/garage/torch/algos/_utils.py
+++ src/garage/torch/algos/_utils.py
@@ -1,10 +1,60 @@
-"""Differentiable utils typically used when computing loss functions."""
+"""Utility functions used by PyTorch algorithms."""
 import torch
 import torch.nn.functional as F
 
 
-def compute_advantages(discount, gae_lambda, max_path_length, baselines,
-                       rewards):
+class _Default:  # pylint: disable=too-few-public-methods
+    """A wrapper class to represent default arguments.
+
+    Args:
+        val (object): Argument value.
+
+    """
+
+    def __init__(self, val):
+        self.val = val
+
+
+def _make_optimizer(optimizer_type, module, **kwargs):
+    """Create an optimizer for PyTorch algos.
+
+    Args:
+        optimizer_type (Union[type, tuple[type, dict]]): Type of optimizer.
+            This can be an optimizer type such as 'torch.optim.Adam' or a
+            tuple of type and dictionary, where dictionary contains arguments
+            to initialize the optimizer e.g. (torch.optim.Adam, {'lr' = 1e-3})
+        module (torch.nn.Module): The module whose parameters needs to be
+            optimized.
+        kwargs (dict): Other keyword arguments to initialize optimizer. This
+            is not used when `optimizer_type` is tuple.
+
+    Returns:
+        torch.optim.Optimizer: Constructed optimizer.
+
+    Raises:
+        ValueError: Raises value error when `optimizer_type` is tuple, and
+            non-default argument is passed in `kwargs`.
+
+    """
+    if isinstance(optimizer_type, tuple):
+        opt_type, opt_args = optimizer_type
+        for name, arg in kwargs.items():
+            if not isinstance(arg, _Default):
+                raise ValueError('Should not specify {} and explicit \
+                    optimizer args at the same time'.format(name))
+        return opt_type(module.parameters(), **opt_args)
+
+    opt_args = {}
+    for name, arg in kwargs.items():
+        if isinstance(arg, _Default):
+            opt_args[name] = arg.val
+        else:
+            opt_args[name] = arg
+    return optimizer_type(module.parameters(), **opt_args)
+
+
+def _compute_advantages(discount, gae_lambda, max_path_length, baselines,
+                        rewards):
     """Calculate advantages.
 
     Advantages are a discounted cumulative sum.
@@ -64,7 +114,7 @@ def compute_advantages(discount, gae_lambda, max_path_length, baselines,
     return advantages
 
 
-def pad_to_last(nums, total_length, axis=-1, val=0):
+def _pad_to_last(nums, total_length, axis=-1, val=0):
     """Pad val to last in nums in given axis.
 
     length of the result in given axis should be total_length.
@@ -95,7 +145,7 @@ def pad_to_last(nums, total_length, axis=-1, val=0):
     return F.pad(tensor, padding_config)
 
 
-def filter_valids(tensor, valids):
+def _filter_valids(tensor, valids):
     """Filter out tensor using valids (last index of valid tensors).
 
     valids contains last indices of each rows.
